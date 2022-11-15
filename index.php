9fbef6dfa289d8c1863fe69cdf453aebe5811294
carolzhang1998 <112989564+carolzhang1998@users.noreply.github.com>
2022-11-15 01:05:05 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<title>Store Home Page</title>
+	<? include "parts/meta.php"; ?>	
+	<script src="http://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+	<? include "parts/navbar.php"; ?>
+	<div class="container">
+		<div class="card soft">
+
+			<h2>Home Page</h2>
+			<p>Create a home page here</p>
+		</div>
+	</div>
+	
+</body>
+</html>
