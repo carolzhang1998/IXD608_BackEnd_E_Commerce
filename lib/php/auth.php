98f6d346a6f515a4f05d0d770555e4263f70c44b
carolzhang1998 <112989564+carolzhang1998@users.noreply.github.com>
2022-11-15 01:07:59 -0800

    Add files via upload

---
+++ lib/php/auth.php
@@ -0,0 +1,16 @@
+<?
+
+function MYSQLIAuth() {
+	return [
+		"localhost", // localhost
+		"carolzhang", // your database user name
+		"Zyqiu980927", // database password
+		"ixd608_store" // database name
+	];
+}
+
+
+
+
+
+?>
--- parts/meta.php
+++ parts/meta.php
@@ -1,5 +1,7 @@
 	<meta charset="UTF-8">
+	<base href="http://yzhang242.net/aau/ixd608/zhang.carol/">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" href="lib/css/styleguide.css">
 	<link rel="stylesheet" href="css/storetheme.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
